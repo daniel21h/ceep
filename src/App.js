5b707fb414653c3a8a9b3b1a9c5563ccb3cba39f
daniel21h <hesseldaniel21@gmail.com>
2020-09-19 19:46:44 -0300

    feat: passing properties

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NoteList } from './components/NoteList';
 import { Form } from './components/Form';
 
 import './index.css'
 
-function App() {
-  return (
-    <section>
-      <Form />
-      <NoteList />
-    </section>
-  );
+class App extends Component {
+  createNote(title, text) {
+
+  }
+
+  render() {
+    return (
+      <section>
+        <Form createNote={this.createNote} />
+        <NoteList />
+      </section>
+    );
+  }
 }
 
 export default App;
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react'
 
 export class Form extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.title = ''
+    this.text = ''
   }
 
-  handleTitleChange(event) {
+  _handleTitleChange(event) {
+    event.stopPropagation()
     this.title = event.target.value
+  }
+
+  _handleTextChange(event) {
+    event.stopPropagation()
+    this.text = event.target.value
+  }
+
+  _createNote(event) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    this.props.createNote(this.title, this.text)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this._createNote.bind(this)}>
         <input
           type="text"
           placeholder="Título"
-          onChange={this.handleTitleChange.bind(this)}
+          onChange={this._handleTitleChange.bind(this)}
         />
 
-        <textarea placeholder="Escreva sua nota..."></textarea>
+        <textarea
+          placeholder="Escreva sua nota..."
+          onChange={this._handleTextChange.bind(this)}
+        />
 
         <button type="submit">Criar nota</button>
       </form>
